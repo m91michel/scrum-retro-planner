4e2302296a8995548637f8c09bdeff879c053b55
Mathias Michel <m91michel@gmail.com>
2019-02-25 08:00:55 +0100

    first step

--- src/app/components/activity-card-list/activity-card-list.component.spec.ts
+++ src/app/components/activity-card-list/activity-card-list.component.spec.ts
@@ -1,17 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCardModule, MatIconModule, MatChipsModule } from '@angular/material';
+import { ActivityService } from '../../services/activity.service';
+import { ActivityListService } from '../../services/activity-list.service';
 import { Activity } from '../../models/model';
 
 import { ActivityCardListComponent } from './activity-card-list.component';
+import { DebugElement } from '@angular/core';
+import { ActivityCardComponent } from '../activity-card/activity-card.component';
 
-xdescribe('ActivityCardListComponent', () => {
+fdescribe('ActivityCardListComponent', () => {
   let component: ActivityCardListComponent;
   let fixture: ComponentFixture<ActivityCardListComponent>;
+  let debugElement: DebugElement;
+  let activityService: ActivityService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActivityCardListComponent ]
+      declarations: [ ActivityCardListComponent, ActivityCardComponent ],
+      imports: [ MatCardModule, MatIconModule, MatChipsModule ],
+      providers: [
+        ActivityService,
+        ActivityListService
+      ]
     })
     .compileComponents();
+
+    debugElement = fixture.debugElement;
+    activityService = debugElement.injector.get(ActivityService);
   }));
 
   beforeEach(() => {

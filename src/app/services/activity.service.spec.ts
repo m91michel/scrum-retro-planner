823e9496411dbf3b50eef20ffced6e4b0759e498
Mathias Michel <m91michel@gmail.com>
2019-03-19 18:46:03 +0100

    format of ActivityService

--- src/app/services/activity.service.spec.ts
+++ src/app/services/activity.service.spec.ts
@@ -1,47 +1,46 @@
 import { TestBed, getTestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-
 import { ActivityService } from './activity.service';
-// https://medium.com/netscape/testing-with-the-angular-httpclient-api-648203820712
+
 describe('ActivityService', () => {
-  let injector: TestBed;
-  let service: ActivityService;
-  let httpMock: HttpTestingController;
+    let injector: TestBed;
+    let service: ActivityService;
+    let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule],
-        providers: [ActivityService]
-    });
-    injector = getTestBed();
-    service = injector.get(ActivityService);
-    httpMock = injector.get(HttpTestingController);
-  });
-
-  afterEach(() => {
-    httpMock.verify();
-  });
-
-  it('should return an Observable<Activity[]>', () => {
-    const dummyActivity = {results: [
-      {
-        id: 1,
-        title: 'title',
-        subline: 'subline',
-        type: 'CHECK_IN',
-        sourceName: 'source',
-        sourceUrl: 'link',
-        description: 'description'
-      }
-    ]};
-
-    service.getActivities().subscribe(activities => {
-      expect(activities.length).toBe(1);
-      expect(activities).toEqual(dummyActivity.results);
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [ActivityService]
+        });
+        injector = getTestBed();
+        service = injector.get(ActivityService);
+        httpMock = injector.get(HttpTestingController);
     });
 
-    const req = httpMock.expectOne(`./api/activities.json`);
-    expect(req.request.method).toBe('GET');
-    req.flush(dummyActivity);
-  });
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('should return an Observable<Activity[]>', () => {
+        const dummyActivity = {
+            results: [{
+                id: 1,
+                title: 'title',
+                subline: 'subline',
+                type: 'CHECK_IN',
+                sourceName: 'source',
+                sourceUrl: 'link',
+                description: 'description'
+            }]
+        };
+
+        service.getActivities().subscribe(activities => {
+            expect(activities.length).toBe(1);
+            expect(activities).toEqual(dummyActivity.results);
+        });
+
+        const req = httpMock.expectOne(`./api/activities.json`);
+        expect(req.request.method).toBe('GET');
+        req.flush(dummyActivity);
+    });
 });
